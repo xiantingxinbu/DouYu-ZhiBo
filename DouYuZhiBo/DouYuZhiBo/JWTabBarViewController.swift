3dd841b15a744715340aa8a90077cc583cea1a3d
Wang jun <13572150949@163.com>
2017-05-06 02:19:59 +0800

    搭建基本框架

---
+++ DouYuZhiBo/DouYuZhiBo/JWTabBarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JWTabBarViewController.swift
+//  DouYuZhiBo
+//
+//  Created by wang jun on 2017/5/6.
+//  Copyright © 2017年 wang jun. All rights reserved.
+//
+
+import UIKit
+
+class JWTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBar.tintColor = UIColor.orange
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
